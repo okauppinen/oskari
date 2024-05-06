3c088c1926aa5b4c6ce24f7738deb520b7b86d7b
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-05-06 09:55:29 +0300

    add NaN check and logging for color utils

--- src/util.js
+++ src/util.js
@@ -228,6 +228,9 @@ Oskari.util = (function () {
         const bracket = color.indexOf('(');
         if (bracket !== -1) {
             const parts = color.substring(bracket + 1, color.indexOf(')')).split(',');
+            if (parts.length < 3 || parts.some(part => isNaN(part))) {
+                return [];
+            }
             return parts.map((part, i) => {
                 if (i === 3) {
                     return parseFloat(part);
@@ -499,15 +502,19 @@ Oskari.util = (function () {
         return retValue;
     };
 
-    util.getColorBrightness = function (color) {
+    util.getColorBrightness = function (color = '') {
         let r, g, b;
 
         if (color.match(/^rgb/)) {
-            color = color.match(/rgba?\(([^)]+)\)/)[1];
-            color = color.split(/ *, */).map(Number);
-            r = color[0];
-            g = color[1];
-            b = color[2];
+            const colors = color
+                .match(/rgba?\(([^)]+)\)/)?.[1]
+                .split(/ *, */)
+                .map(Number);
+            if (Array.isArray(colors)) {
+                r = colors[0];
+                g = colors[1];
+                b = colors[2];
+            }
         } else if (color[0] === '#' && color.length === 7 || color.length === 9) {
             r = parseInt(color.slice(1, 3), 16);
             g = parseInt(color.slice(3, 5), 16);
@@ -516,6 +523,11 @@ Oskari.util = (function () {
             r = parseInt(color[1] + color[1], 16);
             g = parseInt(color[2] + color[2], 16);
             b = parseInt(color[3] + color[3], 16);
+        }
+
+        if (isNaN(r) || isNaN(g) || isNaN(b)) {
+            log.info(`getColorBrightness failed to parse color parts for '${color}'. Defaults to 'light'.`);
+            return 'light';
         }
 
         const brightness = (r * 299 + g * 587 + b * 114) / 1000;
