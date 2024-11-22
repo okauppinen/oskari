da8dde263e50b0acdfe4e8ed0eb94e6cf5fe3023
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-11-22 15:36:53 +0200

    add options

--- src/react/components/TimeSeries/util/constants.js
+++ src/react/components/TimeSeries/util/constants.js
@@ -15,3 +15,25 @@ export const sliderTypes = {
     YEAR: 'year',
     DATE: 'date'
 };
+
+export const playerDefaults = {
+    speed: 'normal',
+    skip: 'none',
+    animate: false
+};
+
+export const playerSpeed = [
+    { key: 'slow', value: 1000},
+    { key: 'normal', value: 2000},
+    { key: 'fast', value: 3000}
+];
+
+// dayjs durations
+export const playerSkip = [
+    { key: 'none', value: ''},
+    { key: 'minute', value: 'minutes'},
+    { key: 'hour', value: 'hours'},
+    { key: 'day', value: 'days'},
+    { key: 'week', value: 'weeks'},
+    { key: 'month', value: 'months'}
+];
