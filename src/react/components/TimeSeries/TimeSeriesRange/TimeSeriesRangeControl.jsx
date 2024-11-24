13edd4bf71eeb3e4cab591b645021a66c09d7909
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-11-24 14:08:57 +0200

    remove header and update props

--- src/react/components/TimeSeries/TimeSeriesRange/TimeSeriesRangeControl.jsx
+++ src/react/components/TimeSeries/TimeSeriesRange/TimeSeriesRangeControl.jsx
@@ -1,81 +1,28 @@
-import { Controller, ThemeConsumer } from 'oskari-ui/util';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Background } from './styled';
-import { TimeSeriesHeader } from './TimeSeriesHeader';
 import { TimeSeriesRange } from './TimeSeriesRange';
 import { TimeSeriesYear } from './TimeSeriesYear';
-import { getNavigationTheme } from 'oskari-ui/theme/ThemeHelper';
 
-export const TimeSeriesRangeControl = ThemeConsumer(({
-    theme,
-    controller,
-    title,
+export const TimeSeriesRangeControl = ({
+    onChange,
     start,
     end,
-    mode,
     value,
-    dataYears,
-    isMobile,
-    loading,
-    error
+    values
 }) => {
-    const toggleMode = () => {
-        if (mode === 'year') {
-            controller.updateValue([start, value], 'range');
-        } else {
-            controller.updateValue(value[1], 'year');
-        }
-    };
-    const navigationTheme = getNavigationTheme(theme);
-    const textColor = navigationTheme.getTextColor();
-    const backgroundColor = navigationTheme.getNavigationBackgroundColor();
-
-    return (
-        <Background textColor={textColor} backgroundColor={backgroundColor} isMobile={isMobile}>
-            <TimeSeriesHeader
-                iconColor={textColor}
-                toggleMode={() => toggleMode()}
-                title={title}
-                mode={mode}
-                loading={loading}
-                error={error}
-                value={value}
-            />
-            {mode === 'year' && (
-                <TimeSeriesYear
-                    onChange={(val) => controller.updateValue(val)}
-                    start={start}
-                    end={end}
-                    value={value}
-                    dataYears={dataYears}
-                    isMobile={isMobile}
-                    iconColor={textColor}
-                />
-            )}
-            {mode === 'range' && (
-                <TimeSeriesRange
-                    onChange={(val) => controller.updateValue(val)}
-                    start={start}
-                    end={end}
-                    value={value}
-                    dataYears={dataYears}
-                    isMobile={isMobile}
-                />
-            )}
-        </Background>
-    );
-});
+    const Control = Array.isArray(value) ? TimeSeriesRange : TimeSeriesYear;
+    return <Control
+        onChange={onChange}
+        start={start}
+        end={end}
+        value={value}
+        values={values}/>;
+};
 
 TimeSeriesRangeControl.propTypes = {
-    controller: PropTypes.instanceOf(Controller).isRequired,
-    title: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
     start: PropTypes.number.isRequired,
     end: PropTypes.number.isRequired,
-    mode: PropTypes.oneOf(['year', 'range']).isRequired,
     value: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]).isRequired,
-    dataYears: PropTypes.arrayOf(PropTypes.number).isRequired,
-    isMobile: PropTypes.bool.isRequired,
-    error: PropTypes.bool,
-    loading: PropTypes.bool
+    values: PropTypes.arrayOf(PropTypes.number).isRequired
 };
